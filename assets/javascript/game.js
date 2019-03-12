4e550b62fca716ac4954849ea11b47113e8158f2
willmcfall <wmcfall2@gmail.com>
2019-03-12 13:31:38 -0400

    Third Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,57 @@
-// Establishes an inital value of zero for wins and losses
+
+// Creates variable and establishes an inital value of zero for wins
 var numWins = 0;
+// Creates variable and establishes an inital value of zero for losses
 var numLosses = 0;
-var actualWord ="Ricky Moriarty"
-var guessLetter = "" 
+// Creates variable and establishes an inital value of ten for number of guesses
+var numGuesses = 10;
+// Creates variable and establishes an inital value of ten for number of guesses
+var numRemaining = 0;
+
+// Creates variable that holds the letter guessed by the user
+var guessLetter = "";
+
+// Creates a variables that will hold the blank spaces for word to be guessed
+var blankSpaces = "";
+
+
+// Creates an array that holds all words that could be potentially used
+var potentialWord = ["Rick Sanchez","Morty Smith","Summer Smith","Beth Smith","Jerry Smith","Abadango Cluster Princess","Abradolf Lincler","Adjudicator Rick","Agency Director","Alan Rails","Albert Einstein","Alexander","Alien Googah","Alien Morty","Alien Rick","Amish Cyborg",
+"Annie","Antenna Morty","Antenna Rick","Ants in my Eyes Johnson", "Mr Poopy Butthole"];
+
+// Creates randomized word that holds the current word to be guessed
+var actualWord = potentialWord[Math.floor(Math.random() * potentialWord.length)];
 
 // Awaits for user to click button
 function myFunction(){
+console.log(actualWord)
+for (i = 0; i < actualWord.length; i++) {
+  var x = actualWord.charAt(i);
+
+  if (x === " " || x === "/'") {
+    blankSpaces += " - ";
+  } else {
+    blankSpaces += " _ ";
+  }
+}
+
 // Prompts use to guess a letter
-document.getElementById("wins").innerHTML = "Number of wins: " + numWins;
-document.getElementById("loses").innerHTML = "Number of losses: " + numLosses;
-var guessLetter = prompt("Please guess a letter!")
+document.getElementById("wins").innerHTML = "Number of Wins: " + numWins;
+document.getElementById("loses").innerHTML = "Number of Losses: " + numLosses;
+document.getElementById("currentword").innerHTML = "Current Word: " + blankSpaces;
+var guessLetter = prompt("Please guess a letter!");
+document.getElementById("guesses").innerHTML = "Number of Guesses Remaining: " + numGuesses;
+document.getElementById("already").innerHTML = "Letters Already Guessed: " + guessLetter + " , ";
+
 
 }
+
+
+
+
+
+
+
 
 
 
